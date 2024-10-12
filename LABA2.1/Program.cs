5855a35db62894838df06ed2c372e55622b13ca8
Chloe3c <kristianishenko21@gmail.com>
2024-10-12 16:12:09 +0200

    Добавьте файлы проекта.

---
+++ LABA2.1/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Введите x ");
+    bool x = bool.Parse(Console.ReadLine()) ;
+    Console.WriteLine("Введите y ");
+    bool y = bool.Parse(Console.ReadLine());
+    Console.WriteLine("Введите z ");
+    bool z = bool.Parse(Console.ReadLine());
+    bool result = x ^ (!(!y || z)) || y;
+    Console.WriteLine($"{result}");
+}
+catch
+{
+    Console.WriteLine("Введите верно!");
+}
